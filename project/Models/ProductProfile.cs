f84d46569024d6e4be3778439ab25e78651cfe84
164910441@qq.com <164910441@qq.com>
2023-09-08 12:06:38 +0800

    add automapper

---
+++ project/Models/ProductProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using project.Dtos;
+using project.Utility.Helper;
+
+namespace project.Models
+{
+    public class ProductProfile:Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<CreateProductDto, Product>().ForMember(e => e.CreateTime, opt => opt.MapFrom(src => TimestampHelper.ToUnixTimeMilliseconds(DateTime.UtcNow)));
+           
+        }
+    }
+}
