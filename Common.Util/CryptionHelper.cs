173565d206c8bf5b3692a720eeaf43f7dc97be37
164910441@qq.com <164910441@qq.com>
2024-01-04 19:32:51 +0800

    update

---
+++ Common.Util/CryptionHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nest;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Utilities.Encoders;
+
+namespace Common.Util
+{
+    public class CryptionHelper
+    {
+        public static string CalculateSHA3Hash(string input)
+        {
+            string data = input+"pwd";
+            // 使用SHA-3-256算法
+            IDigest digest = new Sha3Digest(256);
+
+            // 将字符串转换为字节数组
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+            // 更新摘要
+            digest.BlockUpdate(inputBytes, 0, inputBytes.Length);
+
+            // 计算摘要值
+            byte[] hashBytes = new byte[digest.GetDigestSize()];
+            digest.DoFinal(hashBytes, 0);
+
+            // 将字节数组转换为十六进制字符串
+            return Hex.ToHexString(hashBytes);
+        }
+    }
+}
--- MicroService/Customer.Service/Customer.Infrastructure/Domain/Customers/CustomerRepository.cs
+++ MicroService/Customer.Service/Customer.Infrastructure/Domain/Customers/CustomerRepository.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Common.Util;
 using Customers.Domain.Customers;
 using Customers.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
@@ -18,11 +20,13 @@ namespace Customers.Infrastructure.Domain.Customers
         }
         public async Task Add(Customer customer)
         {
+            customer.PassWord = CryptionHelper.CalculateSHA3Hash(customer.PassWord);
             await _.AddAsync(customer);
         }
 
         public async Task<Customer> Get(string name, string password)
         {
+            password = CryptionHelper.CalculateSHA3Hash(password);
             return await _.Customers.FirstOrDefaultAsync(x=>x.UserName==name&&x.PassWord == password);
         }
     }
